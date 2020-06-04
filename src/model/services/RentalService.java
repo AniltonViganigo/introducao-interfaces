1b2964fda5993e6a964ef6b007b77de025294ac9
Anilton Viganigo Junior <viganigojr@gmail.com>
2020-06-03 21:59:55 -0300

    Solução com o uso da Interface TaxService.

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -8,15 +8,15 @@ public class RentalService {
 	private Double pricePerHour;
 	private Double pricePerDay;
 
-	private BrazilTaxService brazilTaxService;
+	private TaxService taxService;
 
 	public RentalService() {
 	}
 
-	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService brazilTaxService) {
+	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
 		this.pricePerHour = pricePerHour;
 		this.pricePerDay = pricePerDay;
-		this.brazilTaxService = brazilTaxService;
+		this.taxService = taxService;
 	}
 
 	public Double getPricePerHour() {
@@ -35,12 +35,12 @@ public class RentalService {
 		this.pricePerDay = pricePerDay;
 	}
 
-	public BrazilTaxService getBrazilTaxService() {
-		return brazilTaxService;
+	public TaxService getBrazilTaxService() {
+		return taxService;
 	}
 
-	public void setBrazilTaxService(BrazilTaxService brazilTaxService) {
-		this.brazilTaxService = brazilTaxService;
+	public void setTaxService(TaxService taxService) {
+		this.taxService = taxService;
 	}
 
 	public void processInvoice(CarRental carRental) {
@@ -55,7 +55,7 @@ public class RentalService {
 			basicPayment = Math.ceil(hours / 24) * pricePerDay;
 		}
 		
-		double tax = brazilTaxService.tax(basicPayment);
+		double tax = taxService.tax(basicPayment);
 		
 		carRental.setInvoice(new Invoice(basicPayment, tax));
 	}
